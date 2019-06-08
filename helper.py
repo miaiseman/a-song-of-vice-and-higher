61857732445d8e2cc224414603d7e1f37bb657de
mia <mia.iseman@gmail.com>
2019-06-07 17:22:56 -0700

    add ideas for py file

--- helper.py
+++ helper.py
@@ -13,3 +13,10 @@ def get_keys(path):
     
     with open(path) as f:
         return json.load(f)
+     
+#cvs file that has two columns - one that is a person and one that is the colloquial reference to that person 
+#function that references that list and includes "or" between them? or joins them in some way OR just have the dang list here in this file :) 
+#functions that query mondodb? 
+
+
+
